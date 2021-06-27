f9cd1ae987484f2d42ad3eb34d7d73a8a9f865a5
itsnotlupus <noteverlupus@gmail.com>
2021-06-26 22:06:55 -0500

    image upload fix

--- src/modules/ads.ts
+++ src/modules/ads.ts
@@ -4,7 +4,9 @@ export function removeAds() {
   // ad block
   if (process.env.HACKS != "OFF" && Preferences.get(P.disableNags!)) {
     document
-      .querySelectorAll('iframe:not([src*="captcha"]):not([src*="youtube"])')
+      .querySelectorAll(
+        'iframe:not([src*="captcha"]):not([src*="youtube"]):not([src*="about:blank"])'
+      )
       .forEach(iframe => {
         console.log("removing iframe:", iframe);
         iframe.remove();
