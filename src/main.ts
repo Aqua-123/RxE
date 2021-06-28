a72f4b9dbb87c15dd1d6165e6a9faa7105843a4b
itsnotlupus <noteverlupus@gmail.com>
2021-06-27 19:01:51 -0500

    feature flags tweaks

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,11 @@ import { removeAds } from "./modules/ads";
 import { initNetwork } from "./modules/network";
 
 function init() {
-  log.log(`${U.name} Version ${U.version}`);
+  const flags = [];
+  if (FEATURES.HACKS) flags.push("HACKS");
+  if (FEATURES.P2P) flags.push("P2P");
+  const featureSet = flags.length ? `(${flags})` : "";
+  log.log(`${U.name} Version ${U.version} ${featureSet}`);
   // override some builtin behavior
   accountOverrides();
   // inject network middleware
