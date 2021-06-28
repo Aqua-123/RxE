9f4e1ee95379933e8317e055e1caef4dcf81c327
itsnotlupus <noteverlupus@gmail.com>
2021-06-28 01:06:56 -0500

    bump to version 0.5.0

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,7 +1,7 @@
 export default {
   id: "ritsu-emerald",
   name: "Ritsu x Emerald",
-  version: "0.4.0",
+  version: "0.5.0",
   description: "Custom Emerald Chat themes and fixes.",
   icon:
     "https://static.emeraldchat.com/uploads/picture/image/9529291/Ritsu_Icon.png",
