898b1d73c094f765982c4fc66a3feaa5525b9c42
itsnotlupus <noteverlupus@gmail.com>
2021-06-28 00:22:38 -0500

    fix broken gold badge

---
+++ src/modules/brokenimages/index.ts
@@ -0,0 +1,14 @@
+import tickSVG from "./tick.svg";
+
+// 1. replace /badges/tick.svg broken images with a data: tickSVG URI
+// 2. do something cool with other broken images somehow.
+
+export function renderBrokenImages() {
+  const images = document.querySelectorAll('img[src*="/badges/tick.svg"]');
+  images.forEach(img => {
+    // can't set a data: URI in <img>, blocked by CSP. be violent instead.
+    const parent = img.parentElement;
+    img.outerHTML = tickSVG;
+    parent?.querySelector("svg")?.classList.add("user-badge-tick");
+  });
+}
