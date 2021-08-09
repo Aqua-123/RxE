609f349b3f43786f8b3d2f9cf4db63ca6465c4ef
Itsnotlupus <noteverlupus@gmail.com>
2021-08-09 03:25:14 -0500

    #5: nicer fallback message when sending images

--- src/modules/sendpictures/index.ts
+++ src/modules/sendpictures/index.ts
@@ -1,4 +1,37 @@
 import React from "react";
+import { log } from "~userscripter";
+
+const PLACEHOLDER =
+  "Use Ritsu x Emerald 0.9.0 or newer to see an image instead of this placeholder.";
+const IMG_REGEXP = new RegExp(`^${PLACEHOLDER}(.*)$`);
+
+const b64Set =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+// abuse of https://en.wikipedia.org/wiki/Tags_(Unicode_block)
+const tagSet = Array.from({ length: 65 }, (_, i) =>
+  String.fromCodePoint(i + 0xe0020)
+).join("");
+
+function encode(urlPath: string) {
+  const str = btoa(urlPath);
+  let out = "";
+  for (let pos = 0; pos < str.length; pos += 1) {
+    const i = b64Set.indexOf(str[pos]);
+    if (i === -1) return null;
+    out += tagSet[i * 2] + tagSet[i * 2 + 1];
+  }
+  return out;
+}
+
+function decode(str: string) {
+  let b64 = "";
+  for (let pos = 1; pos < str.length; pos += 2) {
+    const i = tagSet.indexOf(str[pos]);
+    if (i === -1) throw new Error("bad image path");
+    b64 += b64Set[(i - 1) / 2];
+  }
+  return atob(b64);
+}
 
 /**
  * This brings back image sending and receiving capability, but
@@ -44,7 +77,9 @@ export function initSendPictures() {
     const urlChunks = e.url.match(
       /^https:\/\/static.emeraldchat.com\/uploads\/picture\/(.*)$/
     );
-    const message = urlChunks ? `Image: [${btoa(urlChunks[1])}]` : undefined;
+    const message = urlChunks
+      ? `${PLACEHOLDER}${encode(urlChunks[1])}`
+      : undefined;
     App.room.client.speak({
       message,
       picture: e
@@ -55,19 +90,36 @@ export function initSendPictures() {
   // tweaking received picture handling
   const rReceived = Room.prototype.received;
   Room.prototype.received = function received(e) {
+    const emitImage = (path: string) => {
+      rReceived.call(this, {
+        messages: [],
+        user: e.user,
+        picture: {
+          url: `https://static.emeraldchat.com/uploads/picture/${path}`
+        } as EmeraldPicture
+      });
+    };
+
     if (e.messages?.[0]) {
-      const matches = e.messages[0].match(/^Image: \[([a-zA-Z0-9/=-]+)\]$/);
+      const matches = e.messages[0].match(IMG_REGEXP);
       if (matches) {
-        rReceived.call(this, {
-          messages: [],
-          user: e.user,
-          picture: {
-            url: `https://static.emeraldchat.com/uploads/picture/${atob(
-              matches[1]
-            )}`
-          } as EmeraldPicture
-        });
-        return;
+        try {
+          emitImage(decode(matches[1]));
+          return;
+        } catch (err) {
+          log.error(err.message);
+        }
+      }
+      const legacyMatches = e.messages[0].match(
+        /^Image: \[([a-zA-Z0-9/=-]+)\]$/
+      );
+      if (legacyMatches) {
+        try {
+          emitImage(atob(legacyMatches[1]));
+          return;
+        } catch (err) {
+          log.error(err.message);
+        }
       }
     }
     rReceived.call(this, e);
