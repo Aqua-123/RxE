b2d209f7cb4dc72465b2ce67417aa0823faf460a
Itsnotlupus <noteverlupus@gmail.com>
2021-08-20 17:17:32 -0500

    plugin types

---
+++ src/types/rxe.d.ts
@@ -0,0 +1,33 @@
+declare type RxEEvent = {
+  type: string;
+  preventDefault(): void;
+};
+declare type RxERoomEvent = RxEEvent & { room: string };
+declare type RxERoomMessageEvent = RxEEvent & MessageData;
+declare type RxEUserEvent = RxEEvent & EmeraldUser;
+
+declare type RxEEventMap = {
+  "room.join": RxERoomEvent;
+  "room.leave": RxERoomEvent;
+  "room.received": RxERoomMessageEvent;
+  "room.userlist": RxEUserEvent;
+  "user.left": RxERoomMessageEvent;
+  "user.joined": RxERoomMessageEvent;
+  "user.message": RxERoomMessageEvent;
+};
+
+declare type RxE = {
+  version(): string;
+  addEventListener<K extends keyof RxEEventMap>(
+    type: K,
+    listener: (ev: RxEEventMap[K]) => any
+  ): void;
+  addEventListener(type: string, listener: (ev: any) => any): void;
+  removeEventListener<K extends keyof RxEEventMap>(
+    type: K,
+    listener: (ev: RxEEventMap[K]) => any
+  ): void;
+  removeEventListener(type: string, listener: (ev: any) => any): void;
+};
+
+declare const RxE: RxE;
