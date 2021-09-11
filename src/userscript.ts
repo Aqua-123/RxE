842dc785f5fe3661baf56f3b3c70450dd6404125
Itsnotlupus <noteverlupus@gmail.com>
2021-09-10 22:18:40 -0500

    bump version to 0.10.0

--- src/userscript.ts
+++ src/userscript.ts
@@ -2,7 +2,7 @@ export default {
   id: "ritsu-emerald",
   name: "Ritsu x Emerald",
   shortName: "RxE",
-  version: "0.9.0",
+  version: "0.10.0",
   description: "Custom Emerald Chat themes and fixes.",
   icon: "https://static.emeraldchat.com/uploads/picture/image/9529291/Ritsu_Icon.png",
   author: "Fxkleti, Atsos/Ritsu, Aqua, Strawberry.",
