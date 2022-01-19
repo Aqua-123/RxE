fe0e519b6fb6be56f4109637ee7409f8050bc1e1
Aqua-123 <https://Aqua-123@github.com>
2022-01-19 06:54:35 +0530

    Slight fix in load more

--- src/modules/fixloadmore/index.ts
+++ src/modules/fixloadmore/index.ts
@@ -1,38 +1,50 @@
-/* eslint-disable */
+/* eslint-disable  */
 export function initLoadMore() {
     Room.prototype.load_messages = function loadmore(e: number) {
+        e = this.state.messages.length;
         $.ajax({
             type: "GET",
-            url: "/room_load_more?loaded=" + String(e) + "&id=" + this.state.id,
+            url: `/room_load_more?loaded=${e}&id=${this.state.id}`,
             dataType: "json",
-            success: function (this: any, e: any) {
-                e.reverse()
-                for (var t = 0; t < e.length; t++) this.prepend(e[t])
+            success: function (this: any, e: any[]) {
+                e = e.reverse();
+                this.trim_messages();
+                const t = this.state.messages;
+                for (let s = 0; s < e.length; s++) {
+                    t.unshift(e[s]);
+                }
             }.bind(this)
-        })
-    }
-    Room.prototype.switch = function fixmessage_count(this: any, e: any) {
-        this.clear_print(), App.room.join(e.id), this.setState({
-            id: e.id,
-            messages: [],
-            messages_count: 0,
-            typing: null,
-            mode: e.mode || "default"
-        }), "private" == e.mode && ($.ajax({
-            type: "GET",
-            url: "/default_private_messages?id=" + e.id,
-            dataType: "json",
-            success: function (this: any, e: any) {
-                let t = 0;
-                for (t = 0; t < e.messages.length; t++) this.append(e.messages[t]);
+        });
+    };
+    Room.prototype.switch = function fixmessagecount(this: any, e: any) {
+        this.clear_print(),
+            App.room.join(e.id),
+            this.setState({
+                id: e.id,
+                messages: [],
+                messages_count: 0,
+                typing: null,
+                mode: e.mode || "default"
+            }),
+            e.mode == "private" &&
+            ($.ajax({
+                type: "GET",
+                url: `/default_private_messages?id=${e.id}`,
+                dataType: "json",
+                success: function (this: any, e: any) {
+                    for (let t = 0; t < e.messages.length; t++) {
+                        console.log(e.messages[t]);
+                        this.append(e.messages[t]);
+                    }
+                    this.setState({
+                        messages_count: e.messages_count
+                    }),
+                        this.scroll();
+                }.bind(this)
+            }),
                 this.setState({
-                    messages_count: e.messages_count
-                }),
-                    this.scroll()
-            }.bind(this)
-        }), this.setState({
-            left_panel: !0,
-            right_panel: !0
-        }))
-    }
+                    left_panel: !0,
+                    right_panel: !0
+                }));
+    };
 }
