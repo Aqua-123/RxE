047ecc17bf3e3de68808bdbf3c555f0663fe554d
Aqua-123 <https://Aqua-123@github.com>
2022-01-31 16:25:57 +0530

    Fixing userlist sorter

--- src/modules/userlist/index.tsx
+++ src/modules/userlist/index.tsx
@@ -7,7 +7,8 @@ import React from "react";
 import { Preferences, P } from "~src/preferences";
 import T from "~src/text";
 import browserWindow from "~src/browserWindow";
-import { equalsTo, existing, loadCSS, sortBy, sorters } from "~src/utils";
+// import { equalsTo, existing, loadCSS, sortBy, sorters } from "~src/utils";
+import { existing, loadCSS } from "~src/utils";
 import style from "./style.scss";
 
 function lookup() {
@@ -100,6 +101,7 @@ export function initUserList() {
     );
   };
   const rcmSetState = RoomChannelMembers.prototype.setState;
+  /*
   RoomChannelMembers.prototype.setState = function setState(e) {
     if (!e || !("members" in e)) return;
     if (!this.state.members)
@@ -118,6 +120,46 @@ export function initUserList() {
         browserWindow.RxE.dispatchEvent("room.userlist", member);
     });
     rcmSetState.call(this, e);
+  }; */
+
+  // Taking reference from previous version of this code cause above one went shwoop my head :)
+  RoomChannelMembers.prototype.setState = function setState(e) {
+    if (e && "members" in e) {
+      const userSort = Preferences.get(P.userSort);
+      switch (userSort) {
+        default:
+        case "name.asc":
+          e.members = e.members
+            .filter((v) => !!v)
+            .sort((a, b) => a!.display_name.localeCompare(b!.display_name));
+          break;
+        case "name.desc":
+          e.members = e.members
+            .filter((v) => !!v)
+            .sort((a, b) => b!.display_name.localeCompare(a!.display_name));
+          break;
+        case "age.asc":
+          e.members = e.members
+            .filter((v) => !!v)
+            .sort((a, b) => b!.id - a!.id);
+          break;
+        case "age.desc":
+          e.members = e.members
+            .filter((v) => !!v)
+            .sort((a, b) => a!.id - b!.id);
+          break;
+      }
+      const previousMembers = this.state.members;
+      e.members.forEach((member) => {
+        if (
+          !previousMembers ||
+          !previousMembers.find((pm) => pm!.id === member!.id)
+        ) {
+          browserWindow.RxE.dispatchEvent("room.userlist", member);
+        }
+      });
+    }
+    rcmSetState.call(this, e as any);
   };
 
   loadCSS(style);
