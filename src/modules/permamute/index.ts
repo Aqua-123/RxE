acf9bd71eba279b15bb70a09800a95d8fa9b52a7
ebg41 <86263963+ebg41@users.noreply.github.com>
2022-02-02 14:09:52 +0100

    gender & permamute fix

--- src/modules/permamute/index.ts
+++ src/modules/permamute/index.ts
@@ -1,89 +1,112 @@
-/* eslint-disable */
 import React from "react";
-import { ListPreferenceMap, P, Preferences } from "~src/preferences";
+import { ListPreferenceMap } from "~src/listprefcache";
+import { P, Preferences } from "~src/preferences";
 import { setDiff } from "~src/utils";
+
 /**
  * Updates mutes about to be saved.
  */
-export function updateMutes(mute_list: Array<[number, string]>) {
-    const mutes = new Set(mute_list.map(x => x[0]));
-    const mutes_old = new Set(
-        Preferences.get(P.permaMuteList).map(x => x[0])
-    );
-    const { added, removed } = setDiff(mutes_old, mutes);
-    for (const id of added) App.room.mute(id);
-    for (const id of removed) App.room.unmute(id);
+export function updateRoomMutes(permamuteList: Array<[number, string]>) {
+  const permamutes = new Set(permamuteList.map((x) => x[0]));
+  const permamutesOld = new Set(
+    Preferences.get(P.permaMuteList).map((x) => x[0])
+  );
+  const { added, removed } = setDiff(permamutesOld, permamutes);
+  added.forEach((id) => App.room.mute(id));
+  removed.forEach((id) => App.room.unmute(id));
 }
 
 function permaMuteAdd(id: number, name: string) {
-    const muted = new ListPreferenceMap(P.permaMuteList);
-    muted.addItem(id, name);
-    muted.destroy();
+  const muted = new ListPreferenceMap(P.permaMuteList);
+  muted.addItem(id, name);
+  muted.destroy();
 }
 
 function permaMuteRemove(id: number) {
-    const muted = new ListPreferenceMap(P.permaMuteList);
-    muted.removeItem(id);
-    muted.destroy();
+  const muted = new ListPreferenceMap(P.permaMuteList);
+  muted.removeItem(id);
+  muted.destroy();
 }
 
 export function initPermaMute() {
-    App.room.muted.push(...Preferences.get(P.permaMuteList).map(x => x[0]));
-    UserView.prototype.bottom = function permamute() {
-        const id = this.state.user.id
-        let muted = App.room.muted.includes(id);
-        let permamuted = Preferences.get(P.permaMuteList).map(x => x[0]).includes(id)
+  App.room.muted.push(...Preferences.get(P.permaMuteList).map((x) => x[0]));
+  UserView.prototype.bottom = function permamute() {
+    const { id } = this.state.user;
+    const muted = App.room.muted.includes(id);
+    const permamuted = Preferences.get(P.permaMuteList)
+      .map((x) => x[0])
+      .includes(id);
 
-        return React.createElement("div", {
-            className: "user-profile-micro-bottom"
+    return React.createElement(
+      "div",
+      {
+        className: "user-profile-micro-bottom"
+      },
+      React.createElement(
+        "div",
+        {
+          onMouseDown: this.view_profile.bind(this),
+          className: "user-profile-micro-button"
         },
-            React.createElement("div", {
-                onMouseDown: this.view_profile.bind(this),
-                className: "user-profile-micro-button"
-            }, "View Profile"), React.createElement("div", {
-                onMouseDown: this.message.bind(this),
-                className: "user-profile-micro-button"
-            }, "Message"), this.mod_button(),
-            React.createElement("div", {
-                onMouseDown: muted ? this.unmute.bind(this) : this.mute.bind(this),
-                className: "user-profile-micro-button",
-                disabled: permamuted
-            }, muted ? "Unmute" : "Mute"),
-            React.createElement("div", {
-                onMouseDown:
-                    permamuted
-                        ? this.permaunmute.bind(this)
-                        : this.permamute.bind(this),
-                className: "user-profile-micro-button"
-            }, permamuted ? "Perma Unmute" : "Perma Mute"),
-            React.createElement("div", {
-                onMouseDown: this.close.bind(this),
-                className: "user-profile-micro-button"
-            }, "Close")
-        );
-
-    }
-    UserView.prototype.permamute = function () {
-        const { id, display_name } = this.state.user;
-        permaMuteAdd(id, display_name);
-        App.room.mute(id, display_name, "Permamuted by user");
-        // For blockreqs
-        NotificationsReact.update();
-        this.forceUpdate();
-    }
-    UserView.prototype.permaunmute = function () {
-        const { id } = this.state.user;
-        permaMuteRemove(id);
-        App.room.unmute(id);
-        NotificationsReact.update();
-        this.forceUpdate();
-    }
-    // TODO: belongs in a more general file
-    UserView.prototype.exit_click = function (e) {
-        if (!(e.target instanceof HTMLElement)) return;
-        if (!e.target.matches('.user-profile-micro, .user-profile-micro *'))
-            this.close();
-    }
-
+        "View Profile"
+      ),
+      React.createElement(
+        "div",
+        {
+          onMouseDown: this.message.bind(this),
+          className: "user-profile-micro-button"
+        },
+        "Message"
+      ),
+      this.mod_button(),
+      React.createElement(
+        "div",
+        {
+          onMouseDown: muted ? this.unmute.bind(this) : this.mute.bind(this),
+          className: "user-profile-micro-button",
+          disabled: permamuted
+        },
+        muted ? "Unmute" : "Mute"
+      ),
+      React.createElement(
+        "div",
+        {
+          onMouseDown: permamuted
+            ? this.permaunmute.bind(this)
+            : this.permamute.bind(this),
+          className: "user-profile-micro-button"
+        },
+        permamuted ? "Perma Unmute" : "Perma Mute"
+      ),
+      React.createElement(
+        "div",
+        {
+          onMouseDown: this.close.bind(this),
+          className: "user-profile-micro-button"
+        },
+        "Close"
+      )
+    );
+  };
+  UserView.prototype.permamute = function permamute() {
+    const { id, display_name: displayName } = this.state.user;
+    permaMuteAdd(id, displayName);
+    App.room.mute(id, displayName, "Permamuted by user");
+    // For blockreqs
+    NotificationsReact.update();
+    this.forceUpdate();
+  };
+  UserView.prototype.permaunmute = function permaunmute() {
+    const { id } = this.state.user;
+    permaMuteRemove(id);
+    App.room.unmute(id);
+    NotificationsReact.update();
+    this.forceUpdate();
+  };
+  // TODO: belongs in a more general file
+  UserView.prototype.exit_click = function exitClick(e) {
+    if (!(e.target instanceof HTMLElement)) return;
+    if (!e.target.matches(".user-profile-micro, .user-profile-micro *"))
+      this.close();
+  };
 }
-
