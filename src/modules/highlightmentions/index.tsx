63a0f6af46ba98e540c39817b152ab70291a6dfc
ebg41 <86263963+ebg41@users.noreply.github.com>
2022-02-09 21:16:25 +0100

    Whole name highlight added

--- src/modules/highlightmentions/index.tsx
+++ src/modules/highlightmentions/index.tsx
@@ -4,27 +4,33 @@ import { P, Preferences } from "~src/preferences";
 import { wrapStringPartitions } from "~src/utils";
 
 const nameNormalized = () =>
-    App.user.display_name.replace(/[^\w\s]/g, "").trim();
+  App.user.display_name.replace(/[^\w\s]/g, "").trim();
 
 const makeMention = (string: string) =>
-    string ? <Flair data={{ string, flair: App.user.flair }} /> : null;
+  string ? <Flair data={{ string, flair: App.user.flair }} /> : null;
 
 export function init() {
-    const mpProcess = Message.prototype.process;
-    Message.prototype.process = function process(message: string) {
-        const processOld = mpProcess.bind(this);
-        const hasEmbed =
-            message.includes("youtu.be") || message.includes("youtube.com");
-        const name = nameNormalized();
-        if (!name || hasEmbed || !Preferences.get(P.highlightMentions))
-            return processOld(message);
-        const messageContent = wrapStringPartitions(
-            message,
-            name,
-            makeMention,
-            processOld
-        );
-        // return React.createElement("span", null, ...messageContent);
-        return messageContent;
-    };
+  const mpProcess = Message.prototype.process;
+  Message.prototype.process = function process(message: string) {
+    const processOld = mpProcess.bind(this);
+    const hasEmbed =
+      message.includes("youtu.be") || message.includes("youtube.com");
+    const name = nameNormalized();
+    if (!name || hasEmbed || !Preferences.get(P.highlightMentions))
+      return processOld(message);
+    const messageContent = wrapStringPartitions(
+      message,
+      name,
+      makeMention,
+      (rest) =>
+        wrapStringPartitions(
+          rest,
+          App.user.display_name,
+          makeMention,
+          processOld
+        )
+    );
+    // return React.createElement("span", null, ...messageContent);
+    return messageContent;
+  };
 }
