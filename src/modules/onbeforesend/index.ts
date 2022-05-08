d26b2b714b16720a08aa0e2fe4a07b4e11dd210d
ebg41 <86263963+ebg41@users.noreply.github.com>
2022-05-08 10:04:59 +0200

    Change emotes to use dot prefix

--- src/modules/onbeforesend/index.ts
+++ src/modules/onbeforesend/index.ts
@@ -1,12 +1,26 @@
 import { links } from "~src/meta";
 import { sanitizeURL } from "../richtext/linkutils";
-import { processEmotes } from "../emotes";
+
+const emotes = {
+  shrug: "¯\\_(ツ)_/¯",
+  tableflip: "(╯°□°）╯︵ ┻━┻",
+  tableflip2: "(ノಠ益ಠ)ノ彡┻━┻",
+  unflip: "┬──┬ ノ( ゜-゜ノ)"
+};
 
 const commands = {
   all: {
     rxelink: () =>
       RoomClient!.send(`Link to RxE extension for Emerald: ${links.repo}`)
   } as Record<string, AnyFunction>,
+  on(command: string) {
+    return {
+      run: (action: AnyFunction) => {
+        commands.all[command] = action;
+        return commands;
+      }
+    };
+  },
   prefix: ".",
   process(message: string) {
     if (!message.startsWith(commands.prefix)) return false;
@@ -31,7 +45,15 @@ const commands = {
   }
 };
 
+function addSubstitutions(substitutions: Record<string, string>) {
+  Object.entries(substitutions).forEach(([name, result]) =>
+    commands.on(name).run(() => RoomClient!.send(result))
+  );
+}
+
 export function init() {
+  addSubstitutions(emotes);
+
   Room.prototype.send = function send(rawMessage: string) {
     const message = this.process ? this.process(rawMessage) : rawMessage;
     if (message === null) return;
@@ -42,13 +64,10 @@ export function init() {
     App.room.client.speak({ message });
     this.scroll();
   };
-  Room.prototype.process = function process(messageRaw) {
-    let processedMessage = messageRaw;
-    processedMessage = processEmotes(messageRaw);
-    if (commands.process(processedMessage)) return null;
-    if (commands.checkMail(processedMessage))
-      return commands.processMail(processedMessage);
-    const message = sanitizeURL(processedMessage);
-    return message;
+
+  Room.prototype.process = function process(message) {
+    if (commands.process(message)) return null;
+    if (commands.checkMail(message)) return commands.processMail(message);
+    return sanitizeURL(message);
   };
 }
