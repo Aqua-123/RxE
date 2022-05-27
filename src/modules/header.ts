f1cb5d34950acb31b997c54a0eb59a4b9a4b5015
Aqua <86239876+Aqua-123@users.noreply.github.com>
2022-05-27 17:04:35 +0530

    Break em loegs

--- src/modules/header.ts
+++ src/modules/header.ts
@@ -3,38 +3,67 @@ import U from "~src/userscript";
 import { crel } from "~src/utils";
 import { getDisplayPicture } from "./altpfp/interceptUser";
 
-export function decorateHeader() {
-  // replace logo
-  const logo = document.querySelector(".main-logo");
+const logo = document.querySelector(".main-logo");
+function setLogo() {
   const displayPicture = getDisplayPicture(App.user);
-  if (logo instanceof HTMLImageElement && logo.src !== displayPicture) {
-    logo.src = displayPicture;
-  }
-  // set favicon
-  if (!document.head.querySelector(`link[rel="icon"][href="${U.icon}"]`)) {
-    document.head
-      .querySelectorAll(`link[rel="icon"]`)
-      .forEach((node) => node.remove());
-    const favicon = crel("link", {
-      rel: "icon",
-      href: U.icon
-    });
-    document.head.prepend(favicon);
-  }
-  // set title
+  if (!(logo instanceof HTMLImageElement) || logo.src === displayPicture)
+    return;
+  logo.src = displayPicture;
+}
+function setFavicon() {
+  if (document.head.querySelector(`link[rel="icon"][href="${U.icon}"]`)) return;
+  document.head
+    .querySelectorAll(`link[rel="icon"]`)
+    .forEach((node) => node.remove());
+  const favicon = crel("link", {
+    rel: "icon",
+    href: U.icon
+  });
+  document.head.prepend(favicon);
+}
+
+function addKarmaPlaceholder() {
+  const karmaTracker = document.querySelector(".karma-tracker");
+  if (karmaTracker) return;
+  const tracker = crel("div", {
+    className: "karma-tracker"
+  });
+  logo?.parentElement?.insertBefore(tracker, logo?.nextSibling);
+}
+
+function fullScreenButton() {
+  const iconsHolder = document.querySelector(".navigation-notification-icons");
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (
+    !document.fullscreenEnabled ||
+    !iconsHolder?.children ||
+    Array.from(iconsHolder?.children).some((child) =>
+      child?.textContent?.includes("full")
+    ) ||
+    isMobile
+  )
+    return;
+  const fullscreenIcon = crel("span", {
+    className: "material-icons navigation-notification-unit",
+    textContent: "open_in_full",
+    onmousedown: async () => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen();
+        fullscreenIcon.textContent = "open_in_full";
+      } else {
+        await document.body.requestFullscreen();
+        fullscreenIcon.textContent = "close_fullscreen";
+      }
+    }
+  });
+  const icon = crel("span");
+  icon.append(fullscreenIcon);
+  iconsHolder.prepend(icon);
+}
+function addTextToLogo() {
   const displayName = App.user.display_name ?? "(...)";
   const title = `${displayName} - ${U.shortName} ${U.version}`;
   if (document.title !== title) document.title = title;
-
-  // add karma placeholder
-  const karmaTracker = document.querySelector(".karma-tracker");
-  if (!karmaTracker) {
-    const tracker = crel("div", {
-      className: "karma-tracker"
-    });
-    logo?.parentElement?.insertBefore(tracker, logo?.nextSibling);
-  }
-  // add text next to logo
   const logoText = document.querySelector(".main-logo-text");
   if (!logoText) {
     const text = crel("div", {
@@ -45,33 +74,16 @@ export function decorateHeader() {
   } else if (logoText.textContent !== title) {
     logoText.textContent = title;
   }
+}
+
+export function decorateHeader() {
+  if (!logo) return;
+  setLogo();
+  setFavicon();
+  // add karma placeholder
+  addKarmaPlaceholder();
+  // add text next to logo and add title
+  addTextToLogo();
   // add fullscreen button
-  const iconsHolder = document.querySelector(".navigation-notification-icons");
-  // check if using mobile
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  if (
-    document.fullscreenEnabled &&
-    iconsHolder?.children &&
-    !Array.from(iconsHolder?.children).some((child) =>
-      child?.textContent?.includes("full")
-    ) &&
-    !isMobile
-  ) {
-    const fullscreenIcon = crel("span", {
-      className: "material-icons navigation-notification-unit",
-      textContent: "open_in_full",
-      onmousedown: async () => {
-        if (document.fullscreenElement) {
-          document.exitFullscreen();
-          fullscreenIcon.textContent = "open_in_full";
-        } else {
-          await document.body.requestFullscreen();
-          fullscreenIcon.textContent = "close_fullscreen";
-        }
-      }
-    });
-    const icon = crel("span");
-    icon.append(fullscreenIcon);
-    iconsHolder.prepend(icon);
-  }
+  fullScreenButton();
 }
--- src/modules/reordermenu.ts
+++ src/modules/reordermenu.ts
@@ -4,10 +4,8 @@ export function reorderMenu() {
   const gold = document
     .evaluate("//li[text()='Emerald Gold']", document)
     .iterateNext();
-  if (Preferences.get(P.adBlocker)) {
-    if (gold instanceof HTMLElement) {
-      gold.remove();
-    }
+  if (Preferences.get(P.adBlocker) && gold instanceof HTMLElement) {
+    gold.remove();
   } else if (gold && gold?.parentElement?.firstChild === gold) {
     gold.parentElement.append(gold);
   }
