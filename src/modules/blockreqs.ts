3e396e3bf022fc01b4d8b7ab0e1988da81f2ca71
Aqua <86239876+Aqua-123@users.noreply.github.com>
2022-06-18 03:58:54 +0530

    Linting

--- src/modules/blockreqs.ts
+++ src/modules/blockreqs.ts
@@ -1,37 +1,40 @@
-/* eslint-disable prettier/prettier */
-
 import { P, Preferences, RequestBlockMode } from "~src/preferences";
 import { DAY, timeSince } from "~src/utils";
 
 function filterNotifications(data: NotificationsStateData) {
-    const blockMode = Preferences.get(P.blockReqs);
-    data.friend_requests = data.friend_requests.filter((request: FriendRequest) => {
-        const joinDate = new Date(request.data.sender.created_at);
-        const blockNew = blockMode !== RequestBlockMode.None && timeSince(joinDate) < DAY;
-        const hide = App ? App.room.muted.includes(request.sender_id) : request.seen;
-        if (!blockNew && !hide) return true;
-        if (blockMode !== RequestBlockMode.Reject) return false;
-        $.ajax({
-            type: "GET",
-            url: `/friends_decline?friend_id=${request.sender_id}&notification_id=${request.id}`,
-            dataType: "json"
-        });
-        return false;
-    });
+  const blockMode = Preferences.get(P.blockReqs);
+  data.friend_requests = data.friend_requests.filter(
+    (request: FriendRequest) => {
+      const joinDate = new Date(request.data.sender.created_at);
+      const blockNew =
+        blockMode !== RequestBlockMode.None && timeSince(joinDate) < DAY;
+      const hide = App
+        ? App.room.muted.includes(request.sender_id)
+        : request.seen;
+      if (!blockNew && !hide) return true;
+      if (blockMode !== RequestBlockMode.Reject) return false;
+      $.ajax({
+        type: "GET",
+        url: `/friends_decline?friend_id=${request.sender_id}&notification_id=${request.id}`,
+        dataType: "json"
+      });
+      return false;
+    }
+  );
 }
 
 function update() {
-    $.ajax({
-        type: "GET",
-        url: "/notifications_json",
-        dataType: "json",
-        // eslint-disable-next-line no-shadow
-        success: function success(data: NotificationsStateData) {
-            filterNotifications(data);
-            NotificationsReact.setState({ data });
-        }
-    });
-    return "updated";
+  $.ajax({
+    type: "GET",
+    url: "/notifications_json",
+    dataType: "json",
+    // eslint-disable-next-line no-shadow
+    success: function success(data: NotificationsStateData) {
+      filterNotifications(data);
+      NotificationsReact.setState({ data });
+    }
+  });
+  return "updated";
 }
 
 /*
@@ -58,11 +61,11 @@ function content(this: NotificationUnit) {
 */
 
 export async function early() {
-    Notifications.prototype.update = update;
-    if (!NotificationsReact) return;
-    NotificationsReact.update = update;
-    if (NotificationsReact.state && NotificationsReact.state.data)
-        filterNotifications(NotificationsReact.state.data);
-    else console.warn('failed to filter friend requests early')
-    // NotificationUnit.prototype.content = content;
+  Notifications.prototype.update = update;
+  if (!NotificationsReact) return;
+  NotificationsReact.update = update;
+  if (NotificationsReact.state && NotificationsReact.state.data)
+    filterNotifications(NotificationsReact.state.data);
+  else console.warn("failed to filter friend requests early");
+  // NotificationUnit.prototype.content = content;
 }
