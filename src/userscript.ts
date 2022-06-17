73800952fbc34da6c63fc5f717750d75b5f2c823
Aqua <86239876+Aqua-123@users.noreply.github.com>
2022-06-18 03:55:53 +0530

    Version Bump 0.11.1

--- src/userscript.ts
+++ src/userscript.ts
@@ -2,7 +2,7 @@ export default {
   id: "ritsu-emerald",
   name: "Ritsu x Emerald",
   shortName: "RxE",
-  version: "0.11.0",
+  version: "0.11.1",
   description: "Custom Emerald Chat themes and fixes.",
   icon: "https://i.imgur.com/KLtVhho.png",
   author: "Atsos/Ritsu, Aqua, Strawberry.",
