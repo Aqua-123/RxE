a5a8a3142001fd980d6499a1bab298f71c2d6739
ebg41 <86263963+ebg41@users.noreplay.github.com>
2022-06-30 06:23:21 +0200

    Bump to 0.11.2

--- src/userscript.ts
+++ src/userscript.ts
@@ -2,7 +2,7 @@ export default {
   id: "ritsu-emerald",
   name: "Ritsu x Emerald",
   shortName: "RxE",
-  version: "0.11.1.1", // also check package.json
+  version: "0.11.2", // also check package.json
   description: "Custom Emerald Chat themes and fixes.",
   icon: "https://i.imgur.com/KLtVhho.png",
   author: "Atsos/Ritsu, Aqua, Strawberry.",
