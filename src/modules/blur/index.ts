087d8f0790bb97d06b7ea4e9e93453818daad1f4
Aqua <86239876+Aqua-123@users.noreply.github.com>
2022-08-17 23:46:40 +0530

    #75 link to pic embeds now comply with imgblur

--- src/modules/blur/index.ts
+++ src/modules/blur/index.ts
@@ -2,16 +2,22 @@ import { loadCSS } from "~src/utils";
 import blurCSS from "./style.scss";
 
 export function applyBlurs() {
-  const arrElements = document.querySelectorAll(
-    ".navigation-notification-unit-image"
-  ) as NodeListOf<HTMLImageElement>;
-  arrElements.forEach((element) => {
-    if (
-      element.src.includes("robohash") ||
-      element.classList.contains("ritsu-would-blur")
-    )
-      return;
-    element.classList.add("ritsu-would-blur");
+  const toBeBlurred = [
+    "room-component-message-picture",
+    "navigation-notification-unit-image"
+  ];
+  toBeBlurred.forEach((className) => {
+    const arrElements = document.querySelectorAll(
+      `.${className}`
+    ) as NodeListOf<HTMLImageElement>;
+    arrElements.forEach((element) => {
+      if (
+        element.src.includes("robohash") ||
+        element.classList.contains("ritsu-would-blur")
+      )
+        return;
+      element.classList.add("ritsu-would-blur");
+    });
   });
 }
 
