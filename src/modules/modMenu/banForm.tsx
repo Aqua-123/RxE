3e03f5f8f11b597b6f31db37e72b7b7cdb323cfd
Aqua <86239876+Aqua-123@users.noreply.github.com>
2023-04-06 12:43:47 +0530

    more ban options

--- src/modules/modMenu/banForm.tsx
+++ src/modules/modMenu/banForm.tsx
@@ -68,11 +68,14 @@ export class BanForm extends Component<BanFormProps, BanFormState> {
             <option value="120">2 minutes</option>
             <option value="300">5 minutes</option>
             <option value="600">10 minutes</option>
+            <option value="900">15 minutes</option>
             <option value="1800">30 minutes</option>
             <option value="3600">1 hour</option>
             <option value="10800">3 hours</option>
+            <option value="21600">6 hours</option>
+            <option value="43200">12 hours</option>
             <option value="86400">1 day</option>
-            <option value="259200">3 days</option>
+            {/* <option value="259200">3 days</option> */}
             <option value="31556952">Permanently</option>
           </select>
         </label>
