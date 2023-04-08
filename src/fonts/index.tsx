fa9ef830bddb661dfdb362f434dd6f5303ae5063
Teriyaki <96557030+Teriyaki812@users.noreply.github.com>
2023-04-08 00:44:16 -0500

    not again...

--- src/fonts/index.tsx
+++ src/fonts/index.tsx
@@ -1,4 +1,9 @@
 import { P, Preferences } from "~src/preferences";
+import { crel } from "~src/utils";
+import verdana from "./verdana.scss";
+import comicSans from "./comicSans.scss";
+import helvetica from "./helvetica.scss";
+import trebuchet from "./trebuchet.scss";
 
 export type FontsProps = {
   font: Font;
@@ -57,36 +62,66 @@ export type FontLabel =
 //   });
 // }
 
-function changeFont(font: string) {
-  const body = document.getElementsByTagName("body")[0];
-  const size = "14px ";
-  body.style.font = `${size + font}`;
-  const microposts = document.querySelectorAll(
-    ".user-profile-micro"
-  ) as NodeListOf<HTMLElement>;
-  const importantTag = " !important";
-  Array.from(microposts).forEach((element) => {
-    element.style.fontFamily = `${font + importantTag}`;
-  });
-  const menuNotif = document.querySelectorAll(
-    ".notification-menu-container-text"
-  ) as NodeListOf<HTMLElement>;
-  Array.from(menuNotif).forEach((element) => {
-    element.style.fontFamily = font;
-  });
-  const micro = document.querySelectorAll(
-    ".user-micropost-unit"
-  ) as NodeListOf<HTMLElement>;
-  Array.from(micro).forEach((element) => {
-    element.style.fontFamily = font;
-  });
-}
+// function changeFont(font: string) {
+//   const body = document.getElementsByTagName("body")[0];
+//   const size = "14px ";
+//   body.style.font = `${size + font}`;
+//   const microposts = document.querySelectorAll(
+//     ".user-profile-micro"
+//   ) as NodeListOf<HTMLElement>;
+//   const importantTag = " !important";
+//   Array.from(microposts).forEach((element) => {
+//     element.style.fontFamily = `${font + importantTag}`;
+//   });
+//   const menuNotif = document.querySelectorAll(
+//     ".notification-menu-container-text"
+//   ) as NodeListOf<HTMLElement>;
+//   Array.from(menuNotif).forEach((element) => {
+//     element.style.fontFamily = font;
+//   });
+//   const micro = document.querySelectorAll(
+//     ".user-micropost-unit"
+//   ) as NodeListOf<HTMLElement>;
+//   Array.from(micro).forEach((element) => {
+//     element.style.fontFamily = font;
+//   });
+// }
 
 export function initFont() {
   const font = Preferences.get(P.font) as FontLabel;
-  if (!font || font === "Default Font") changeFont("Roboto");
-  // const fontLabel = fontsQuery[font];
-  else changeFont(font);
+  let styleSheet = document.head.querySelector(".custom-font");
+  if (!styleSheet) {
+    document.head.append(
+      crel("style", {
+        className: "custom-font",
+        type: "text/css"
+      })
+    );
+    styleSheet = document.head.querySelector(".custom-theme")!;
+  }
+  const css = [];
+  switch (font) {
+    case "Default Font":
+    default:
+      break;
+    case "Comic Sans MS":
+      css.push(comicSans);
+      break;
+    case "Helvetica":
+      css.push(helvetica);
+      break;
+    case "Trebuchet MS":
+      css.push(trebuchet);
+      break;
+    case "Verdana":
+      css.push(verdana);
+      break;
+  }
+  styleSheet.textContent = css.join("\n");
+  // const font = Preferences.get(P.font) as FontLabel;
+  // if (!font || font === "Default Font") changeFont("Roboto");
+  // // const fontLabel = fontsQuery[font];
+  // else changeFont(font);
   //
   // switch (font.value) {
   //   case "roboto":
