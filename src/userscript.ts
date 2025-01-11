fcfb845669a68acc7c7dbdb535a98640037d7ada
Teriyaki812 <96557030+Teriyaki812@users.noreply.github.com>
2025-01-11 18:11:55 -0500

    End of the line_

--- src/userscript.ts
+++ src/userscript.ts
@@ -2,7 +2,7 @@ export default {
   id: "ritsu-emerald",
   name: "Ritsu x Emerald",
   shortName: "RxE",
-  version: "0.13.5.5", // yarn bump to update version
+  version: "0.13.5.6", // yarn bump to update version
   description: "Custom Emerald Chat themes and fixes.",
   icon: "https://i.imgur.com/14f9VD4.png",
   author: "Atsos/Ritsu, Aqua, Strawberry, Teriyaki",
