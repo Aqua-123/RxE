1cc65f6bf0c61e62d914473cfe48b68a6286e249
Teriyaki <96557030+Teriyaki812@users.noreply.github.com>
2025-06-08 20:19:42 -0500

    New url

--- src/meta/index.ts
+++ src/meta/index.ts
@@ -1,5 +1,5 @@
 export const links = {
   repo_minified: "https://git.io/JuwK0", // works
-  repo: "https://github.com/Ritsu-Projects/RxE-Public-Releases/",
+  repo: "https://github.com/Teriyaki812/RxE-Public-Releases/",
   rules: "https://rentry.co/97eydt"
 };
